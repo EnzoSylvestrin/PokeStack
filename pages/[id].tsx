a352ee2206fab84ae61edb0940729725e5503c5f
EnzoSylvestrin <enzospavani@gmail.com>
2023-01-24 22:51:14 -0300

    Criando tela de details

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,25 +1,174 @@
+import { useEffect, useState } from "react";
+
+import { useRouter } from "next/router";
+
+import { Pokemon, PokemonAbility, PokemonClient } from "pokenode-ts";
+
 import { ThemeProvider } from "styled-components";
+import { theme } from "../styles/Theme";
+
+import { toast } from "react-hot-toast";
+import { Barbell, Circle, Hand, Ruler, Sword } from "phosphor-react";
 
 import BackComponent from "../components/BackComponent";
 import HeadComponent from "../components/HeadCoponent";
+import LoadingComponent from "../components/Loading/LoadingComponent";
 
-import { theme } from "../styles/Theme";
-import { DetailsContainer } from "../styles/DetailsStyled";
-import { useRouter } from "next/router";
+import { DataContainer, DetailsContainer, IconDetails, IconWrapper, TitleContainer, Wrapper } from "../styles/DetailsStyled";
+
+type DataDetails = {
+    imgUrl: string | undefined,
+    name: string,
+    weight: number,
+    exp: number,
+    height: number;
+    abilities: string[],
+    moves: string[]
+}
 
 function Details() {
 
     const { id } = useRouter().query;
 
-    console.log(id);
+    const [data, setData] = useState<DataDetails>();
+
+    const PokeApi = new PokemonClient();
+
+    const ParseId = (id: string | string[] | undefined): number => {
+        if (typeof id === "string") {
+            return parseInt(id);
+        }
+        else {
+            return -1;
+        }
+    }
+
+    const getPokemon = async () => {
+        try {
+            let Pokemon = await PokeApi.getPokemonById(ParseId(id));
+            let Abilitys: string[] = [];
+            let Moves: string[] = [];
+            if (Pokemon.abilities.length > 3) {
+                for (let i = 0; i < 3; i++) {
+                    Abilitys.push(Pokemon.abilities[i].ability.name);
+                }
+            }
+            else {
+                for (let ability of Pokemon.abilities) {
+                    Abilitys.push(ability.ability.name);
+                }
+            }
+            if (Pokemon.moves.length > 3) {
+                for (let i = 0; i < 3; i++) {
+                    Moves.push(Pokemon.moves[i].move.name);
+                }
+            }
+            else {
+                for (let move of Pokemon.moves) {
+                    Moves.push(move.move.name);
+                }
+            }
+            setData(
+                {
+                    imgUrl: Pokemon.sprites.front_default?.toString(),
+                    name: Pokemon.name.toUpperCase(),
+                    weight: Pokemon.weight,
+                    exp: Pokemon.base_experience,
+                    height: Pokemon.height,
+                    abilities: Abilitys,
+                    moves: Moves
+                }
+            );
+        }
+        catch (err) {
+            console.log(err);
+            toast.error("Ocorreu um erro", {
+                position: 'bottom-center'
+            });
+        }
+    }
+
+    useEffect(() => {
+        getPokemon();
+    }, [])
 
     return (
         <ThemeProvider theme={theme}>
             <HeadComponent />
             <DetailsContainer>
-                <BackComponent />
+                {
+                    data != null
+                        ?
+                        <>
+                            <BackComponent />
+                            <TitleContainer
+                                initial={{ y: -200, opacity: 0 }}
+                                whileInView={{ y: 0, opacity: 1 }} //onScroll
+                                transition={{ duration: 0.5 }}
+                            >
+                                <img src={data.imgUrl} alt="imagem Pokemon" />
+                                <h1>{data.name}</h1>
+                            </TitleContainer>
+                            <DataContainer
+                                initial={{ y: 200, opacity: 0 }}
+                                whileInView={{ y: 0, opacity: 1 }} //onScroll
+                                transition={{ duration: 0.5 }}
+                            >
+                                <Wrapper>
+                                    <h1>Status:</h1>
+                                    <IconWrapper>
+                                        <IconDetails>
+                                            <Ruler />
+                                        </IconDetails>
+                                        <p>{`${data.height / 10}m`}</p>
+                                    </IconWrapper>
+                                    <IconWrapper>
+                                        <IconDetails>
+                                            <Circle />
+                                        </IconDetails>
+                                        <p>{`${data.exp} exp`}</p>
+                                    </IconWrapper>
+                                    <IconWrapper>
+                                        <IconDetails>
+                                            <Barbell />
+                                        </IconDetails>
+                                        <p>{`${data.weight / 10}kg`}</p>
+                                    </IconWrapper>
+                                </Wrapper>
+                                <Wrapper>
+                                    <h1>Movimentos:</h1>
+                                    {
+                                        data.moves.map(move => {
+                                            return <IconWrapper>
+                                                <IconDetails>
+                                                    <Sword />
+                                                </IconDetails>
+                                                <p>{move}</p>
+                                            </IconWrapper>
+                                        })
+                                    }
+                                </Wrapper>
+                                <Wrapper>
+                                    <h1>Habilidades:</h1>
+                                    {
+                                        data.abilities.map(ability => {
+                                            return <IconWrapper>
+                                                <IconDetails>
+                                                    <Hand />
+                                                </IconDetails>
+                                                <p>{ability}</p>
+                                            </IconWrapper>
+                                        })
+                                    }
+                                </Wrapper>
+                            </DataContainer>
+                        </>
+                        :
+                        <LoadingComponent w={"350px"} h={"350px"} />
+                }
+
             </DetailsContainer>
-        </ThemeProvider>
+        </ThemeProvider >
     );
 }
 
--- styles/DetailsStyled.tsx
+++ styles/DetailsStyled.tsx
@@ -1,4 +1,7 @@
+import { motion } from "framer-motion";
 import styled from "styled-components";
+
+import { Slot } from '@radix-ui/react-slot';
 
 export const DetailsContainer = styled.div`
     min-height: 100vh;
@@ -6,4 +9,84 @@ export const DetailsContainer = styled.div`
     display: flex;
     align-items: center;
     flex-direction: column;
+`;
+
+export const TitleContainer = styled(motion.div)`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+
+    img {
+        height: 200px;
+        width: 200px;
+    }
+
+    h1 {
+        background: linear-gradient(to right, ${props => props.theme.colors.secondary}, ${props => props.theme.colors.main});
+        -webkit-background-clip: text;
+        color: transparent;
+        font-size: 36px;
+    }
+
+    @media (max-width: 460px) {
+        flex-direction: column;
+
+        h1 {
+            margin: 5px 0;
+        }
+    }
+`;
+
+export const DataContainer = styled(motion.div)`
+    width: 100%;
+    border-radius: 60px 60px 0 0;
+    background-color: ${props => props.theme.colors.lightDark};
+    min-height: 55vh;
+    border: 2px solid ${props => props.theme.colors.secondary};
+    border-bottom: 0px;
+    padding: 20px 30px;
+    display: flex;
+    align-items: center;
+
+    @media (max-width: 800px) {
+        flex-direction: column;
+    }
+`;
+
+export const Wrapper = styled.div`
+    flex: 1;
+    text-align: center;
+    color: ${props => props.theme.colors.dark.textColor};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    h1 {
+        margin-bottom: 15px;
+    }
+
+    @media (max-width: 800px) {
+        margin: 10px 0;
+    }
+`;
+
+export const AbilityWrapper = styled.div`
+`;
+
+export const IconWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    min-width: 120px;
+    gap: 6px;
+    margin: 10px 0;
+    font-size: 22px;
+    color: ${props => props.theme.colors.dark.textColor};
+`;
+
+export const IconDetails = styled(Slot)`
+    height: 32px;
+    width: 32px;
+    color: ${props => props.theme.colors.main};
 `;
